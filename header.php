3f01866c3ed78373f6cd515a6e028c0e700da77b
Fabaow <corentin.famechon@gmail.com>
2018-05-14 16:44:48 +0200

    modification du header.php

--- header.php
+++ header.php
@@ -2,8 +2,18 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
-		<title>Titre de la page</title>
-		<link rel="stylesheet" href="style.css">
+		<title>Ucape</title>
+		<link rel="stylesheet" href="assets/style.css">
 		<script src="script.js"></script>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	</head>
 	<body>
+		<div class="container">
+			<div id="titre">
+				<h1>Ucape - Section</h1>
+				<h4>Bonjour Visiteur !</h4>
+			</div>
+		</div>
