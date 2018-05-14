f9d19f29bc7c7b55f034ae8a00d463f494ef0584
nicoCastro99 <castronico1999@gmail.com>
2018-05-14 15:25:25 +0200

    Modifier fichier db dans includes

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,7 @@
 <?php
 
-	require_once 'login.php';
+	require_once("login.php");
 
-    $dbLogin = "root";
-    $dbPassword = "root";
     $dbName = "ucape_cennz";
-    $connection = new \PDO("mysql:host=127.0.0.1:8889;dbname=$dbName", $user, $pass);
+    $connection = new \PDO("mysql:host=127.0.0.1:8889;dbname=$dbName", $dbLogin, $dbPassword);
 ?>
