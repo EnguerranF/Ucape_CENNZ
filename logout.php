bd9432e43fc950bc193ad3f8f9f8914c872e465e
nicoCastro99 <castronico1999@gmail.com>
2018-05-28 11:05:52 +0200

    Ajout d'un fichier pour la deconnection

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+	require_once('includes/session_start.php');
+    require_once 'includes/header.php';
+
+    if (isset($_SESSION['utilisateur'])) {
+        unset($_SESSION['utilisateur']);
+    }
+
+    //header('Location: index.php');
+    ////////////////////////////////////
+    header('Location: ./index.php');
+	exit();	
+	////////////////////////////////////
+?>
